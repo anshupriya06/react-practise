311a426624d1e09b74ec237dda955e6b9c60820e
Anshu Priya <anshupriyadnr06@gmail.com>
2025-03-02 12:00:00 +0530

    created a React project

---
+++ deno-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Title from "./title.jsx";
+
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <Title />
+    </div>
+  )
+}
+
+export default App
---
+++ deno-react-app/src/title.jsx
@@ -0,0 +1,6 @@
+
+function Title(){
+    return <h2>I am the title</h2>
+  }
+
+export default Title
